e53e5b9c816bd58ae9080b0ee2b1e987764fbdf5
sathishashokan <sathishsharma1990@gmail.com>
2024-06-10 18:47:18 +0530

    commiting third task

--- exercise.rb
+++ exercise.rb
@@ -1,5 +1,6 @@
 def reference_code(code)
     slash_to_dash = code.tr('/', '-')
+    slash_to_dash.upcase! #Change lowercase to uppercase if present
     arr = slash_to_dash.split("-")
     if arr.length == 2
         return code
@@ -13,5 +14,5 @@ def reference_code(code)
 end
 
 puts reference_code("K7DTY-BXV6T")
-puts reference_code("K7DTY/BXV6T/2")
-puts reference_code("K7DTY/BXV6T-3")
+puts reference_code("K7Dty/BXV6T/2")
+puts reference_code("K7DTY/BxV6t-3")
